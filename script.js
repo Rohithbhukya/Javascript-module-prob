79c0e4c29f6a40438f835d75c32b966b5e2d4e14
Rohithbhukya <48701042+Rohithbhukya@users.noreply.github.com>
2024-02-01 23:00:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const cmpScr = document.querySelector("#cmpScr");
+const urScr = document.querySelector("#urScr");
+const rock = document.querySelector("#rock");
+const imgsRock = document.querySelector("#imgsRock")
+const scissor = document.querySelector("#scissor");
+const paper = document.querySelector("#paper");
+const secondMain = document.querySelector(".secondMain");
+const rockWinId = document.querySelector("#rockWinId");
+const exitIcon = document.querySelector("#exitIcon");
+const gameRulesId = document.querySelector("#gameRulesId");
+const rnBtn = document.querySelector("#rnBtn");
+
+
+// rockWinId.style.display = "none";
+rock.addEventListener("click", () => {
+    secondMain.style.display = "none";
+    rockWinId.style.display = "block";
+
+});
+
+exitIcon.addEventListener("click", () => {
+    gameRulesId.style.display = "none";
+});
+
+rnBtn.addEventListener("click", () => {
+    gameRulesId.style.display = "block";
+});
+
